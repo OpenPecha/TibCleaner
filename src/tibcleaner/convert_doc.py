aaba9fa15edffddf1643563377238ef663b2361a
Tenzin <tenzintsunduebhattu@gmail.com>
2024-02-05 12:07:21 +0530

    add_logging_system

--- src/tibcleaner/convert_doc.py
+++ src/tibcleaner/convert_doc.py
@@ -1,8 +1,24 @@
+import logging
 import subprocess
 from pathlib import Path
 
 from tibcleaner.config import DATA_DIR, load_doc_checkpoints, save_doc_checkpoint
 from tibcleaner.filter import get_all_doc_files
+
+log_fn = "doc_to_docx_errors.log"
+error_file_log_fn = "docx_to_docx_error_files.log"
+
+logging.basicConfig(
+    filename=str(log_fn),
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
+def log_error_with_id(file_: str):
+    """Log error message with ID to a separate file."""
+    with open(error_file_log_fn, "a") as log_file:
+        log_file.write(f"{file_}\n")
 
 
 def get_libreoffice_path():
@@ -43,8 +59,13 @@ def covert_doc_files_pipeline(folder_dir: Path, destination_dir: Path):
     doc_checkpoints = load_doc_checkpoints()
     for file in files:
         if file not in doc_checkpoints:
-            convert_doc_to_docx(file, destination_dir)
-            save_doc_checkpoint(file)
+            try:
+                convert_doc_to_docx(file, destination_dir)
+                save_doc_checkpoint(file)
+            except Exception as e:
+                logging.error(f"An error occurred while converting {file.name}: {e}")
+                log_error_with_id(file)
+                continue
 
 
 if __name__ == "__main__":
