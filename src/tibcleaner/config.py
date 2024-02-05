fa0423a0ee4acf967285f98fdf27a4fa25c2497f
Tenzin <tenzintsunduebhattu@gmail.com>
2024-02-05 13:10:23 +0530

    convert_docx_files_pipeline

--- src/tibcleaner/config.py
+++ src/tibcleaner/config.py
@@ -13,17 +13,31 @@ ROOT_DIR = CUR_DIR.parent.parent
 DATA_DIR = _mkdir(ROOT_DIR / "DATA")
 
 BASE_PATH = Path.home()
-CHECKPOINT_FILE = BASE_PATH / "doc_to_docx_checkpoint.txt"
+DOC_CONVERT_CHECKPOINT_FILE = BASE_PATH / "doc_to_docx_checkpoint.txt"
+DOCX_CONVERT_CHECKPOINT_FILE = BASE_PATH / "docx_to_txt_checkpoint.txt"
 
 
 def load_doc_checkpoints():
-    if CHECKPOINT_FILE.exists():
-        return CHECKPOINT_FILE.read_text().splitlines()
+    if DOC_CONVERT_CHECKPOINT_FILE.exists():
+        return DOC_CONVERT_CHECKPOINT_FILE.read_text().splitlines()
 
-    CHECKPOINT_FILE.touch()
+    DOC_CONVERT_CHECKPOINT_FILE.touch()
     return []
 
 
 def save_doc_checkpoint(file_checkpoint):
-    with open(CHECKPOINT_FILE, "a") as f:
+    with open(DOC_CONVERT_CHECKPOINT_FILE, "a") as f:
+        f.write(f"{file_checkpoint}\n")
+
+
+def load_docx_checkpoints():
+    if DOCX_CONVERT_CHECKPOINT_FILE.exists():
+        return DOCX_CONVERT_CHECKPOINT_FILE.read_text().splitlines()
+
+    DOCX_CONVERT_CHECKPOINT_FILE.touch()
+    return []
+
+
+def save_docx_checkpoint(file_checkpoint):
+    with open(DOCX_CONVERT_CHECKPOINT_FILE, "a") as f:
         f.write(f"{file_checkpoint}\n")
